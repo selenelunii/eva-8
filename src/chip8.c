3bf5e722bd0fe7dcc46d2da300082583fe6cdffd
selenelunii <96795974+selenelunii@users.noreply.github.com>
2026-03-28 21:56:01 -0400

    feat: add full ROM loader

--- src/chip8.c
+++ src/chip8.c
@@ -1,8 +1,15 @@
 #include "../include/chip8.h"
 #include <stdio.h>
-int chip8_start(CPU *cpu, display *display, uint8_t *buffer)
+int chip8_start(CPU *cpu, display *display, uint8_t *buffer, int size)
 {
     cpu_start(cpu);
+    memcpy((cpu->ram)+0x200,buffer,size);
+    printf("ROM - START\n");
+    for(int i = 0; i < size; i++)
+    {
+        printf("ram[0x%x] : 0x%x\n",0x200+i,cpu->ram[0x200 + i]);
+    }
+    printf("ROM - END\n");
     display_start(display);
     return 0;
 }
