3e187f51f4fb90513c2affbd2c35557445c12521
selenelunii <96795974+selenelunii@users.noreply.github.com>
2026-04-01 17:32:34 -0400

    feat: add error handling for display initialization

--- src/display.c
+++ src/display.c
@@ -1,13 +1,31 @@
 #include "../include/display.h"
+#include <SDL3/SDL_error.h>
 #include <stdio.h>
 int display_start (display *display)
 {
+    display->quit = true;
     printf("EVA-8 is starting\n");
     SDL_Init(SDL_INIT_VIDEO);
     display->window = SDL_CreateWindow("EVA-8: A(nother) CHIP-8 emulator",WIDTH * FACTOR_DISPLAY ,HEIGHT * FACTOR_DISPLAY,SDL_WINDOW_RESIZABLE);
+    if (display->window == NULL)
+    {
+        fprintf(stderr,"ERROR creating window: %s", SDL_GetError());
+        return 1;
+    }
     display->render = SDL_CreateRenderer(display->window,NULL);
+    if (display->render == NULL)
+    {
+        fprintf(stderr,"ERROR creating render: %s", SDL_GetError());
+        return 1;
+    }
     display->texture = SDL_CreateTexture(display->render,SDL_PIXELFORMAT_XRGB8888,SDL_TEXTUREACCESS_STREAMING,WIDTH,HEIGHT);
+    if (display->texture == NULL)
+    {
+        fprintf(stderr,"ERROR creating texture: %s", SDL_GetError());
+        return 1;
+    }
     display->quit = false;
+    return 0;
 }
 void display_draw (display *display)
 {
