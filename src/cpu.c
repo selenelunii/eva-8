7a2a942f3c032169e0fcb9580b4e44ebcc190ae8
selenelunii <96795974+selenelunii@users.noreply.github.com>
2026-04-19 17:55:50 -0400

    Revert "Feature/arithmetic and logical opcodes"

--- src/cpu.c
+++ src/cpu.c
@@ -54,107 +54,16 @@ void cpu_step(CPU *cpu)
                 case 0xE0:
                     memset(cpu->display, 0x00, sizeof(cpu->display));
                     break;
-                case 0xEE:
-                    cpu->sp--;
-                    cpu->pc = cpu->stack[cpu->sp];
-                    break;
             }
             break;
         case 0x1:
             cpu->pc = NNN;
-            break;
-        case 0x2:
-            cpu->stack[cpu->sp] = cpu->pc;
-            cpu->sp++;
-            cpu->pc = NNN;
-            break;
-        case 0x3:
-            if(cpu->v[X] == NN)
-            {
-                cpu->pc += 2;
-            }
-            break;
-        case 0x4:
-            if(cpu->v[X] != NN)
-            {
-                cpu->pc += 2;
-            }
-            break;
-        case 0x5:
-            if(cpu->v[X] == cpu->v[Y])
-            {
-                cpu->pc += 2;
-            }
             break;
         case 0x6:
             cpu->v[X] = NN;
             break;
         case 0x7:
             cpu->v[X] += NN;
-            break;
-        case 0x8:
-            switch(N)
-            {
-                case 0x0:
-                    cpu->v[X] = cpu->v[Y];
-                    break;
-                case 0x1:
-                    cpu->v[X] |= cpu->v[Y];
-                    break;
-                case 0x2:
-                    cpu->v[X] &= cpu->v[Y];
-                    break;
-                case 0x3:
-                    cpu->v[X] ^= cpu->v[Y];
-                    break;
-                case 0x4:
-                    if((uint16_t) cpu->v[X] + (uint16_t) cpu->v[Y] > 255 )
-                    {
-                        cpu->v[0xF] = 1;
-                    }
-                    else
-                    {
-                        cpu->v[0xF] = 0;
-                    }
-                    cpu->v[X] += cpu->v[Y];
-                    break;
-                case 0x5:
-                    if(cpu->v[X] >= cpu->v[Y])
-                    {
-                        cpu->v[0xF] = 1;
-                    }
-                    else
-                    {
-                        cpu->v[0xF] = 0;
-                    }
-                    cpu->v[X] = cpu->v[X] - cpu->v[Y];
-                    break;
-                case 0x6:
-                    cpu->v[X] = cpu->v[Y] >> 1;
-                    cpu->v[0xF] = cpu->v[Y] & 0x1;
-                    break;
-                case 0x7:
-                    if(cpu->v[Y] >= cpu->v[X])
-                    {
-                        cpu->v[0xF] = 1;
-                    }
-                    else
-                    {
-                        cpu->v[0xF] = 0;
-                    }
-                    cpu->v[X] = cpu->v[Y] - cpu->v[X];
-                    break;
-                case 0xE:
-                    cpu->v[X] = cpu->v[Y] << 1;
-                    cpu->v[0xF] = (cpu->v[Y] & 0x80) >> 7;
-                    break;
-            }
-            break;
-        case 0x9:
-            if(cpu->v[X] != cpu->v[Y])
-            {
-                cpu->pc += 2;
-            }
             break;
         case 0xA:
             cpu->i = NNN;
@@ -170,7 +79,7 @@ void cpu_step(CPU *cpu)
                     if ((pixel_x + row < 64) && (pixel_y + col < 32))
                     {
                         pixel = (cpu->ram[cpu->i + col] >> (7 - row)) &  0x01;
-                        if ((cpu->display[(pixel_x + row) + ((pixel_y + col) * 64)] == 1) && (pixel) == 1)
+                        if ((cpu->display[(pixel_x + row) + (pixel_y + col * 64)] == 1) && (pixel) == 1)
                         {
                             cpu->v[0x0F] = 1;
                         }
